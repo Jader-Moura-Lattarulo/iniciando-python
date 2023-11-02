0ef793637be4bfbea8582aa6ead9e57c87c7c6d0
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2023-11-02 16:48:50 -0300

    Add files via upload
    Conjunto de 08 exercícios simples:
    Antecessor e sucessor de um número dado pelo usuário;
    Dobro, triplo e raiz de um número dado pelo usuário;
    Nome, notas e média de um aluno;
    Conversor metro para centímetro e milímetro;
    Tabuada de 1 a 10 com o número fornecido pelo usuário;
    Conversor real para dólar;
    Calculo de metragem da parede e quanto de tinta será necessário para pinta-la;
    Desconto de 5% em um produto;
    Acréscimo de 15% ao salário.

---
+++ ex003.py
@@ -0,0 +1,84 @@
+des = ' DESAFIO 05 '
+print('{:=^30}'.format(des))
+print('Qual número você quer saber o antecessor e o sucessor? ', end='')
+numero = int(input())
+ant = numero -1
+suc = numero +1
+print('O antecessor de {} é {};'.format(numero, ant))
+print('Seu sucessor é {}.\n\n'.format(suc))
+
+des = ' DESAFIO 06 '
+print('{:+^30}'.format(des))
+print('Qual número você quer saber o dobro, triplo e raiz quadrada? ', end='')
+numero = int(input())
+dob = numero * 2
+tri = numero *3
+raiz = numero **0.5
+print('O dobro é: {}, o triplo é: {} e a raiz quadrada é: {}\n\n'.format(dob, tri, raiz))
+
+des = ' DESAFIO 07 '
+print('{:!^30}'.format(des))
+print('Insira o nome do aluno:', end=' ')
+nome = str(input())
+print('Insira foi a primeira nota:', end=' ')
+nota01 = float(input())
+print('Insira foi a segunda nota:', end=' ')
+nota02 = float(input())
+
+media = (nota01 + nota02)/2
+
+print('O aluno {}, tirou as notas {} e {} e ficou com a média final: {:.2f}\n\n'.format(nome, nota01, nota02, media))
+
+des = ' DESAFIO 08 '
+print('{:#^30}'.format(des))
+print('Quantos metros você quer converter para centimetros e milímetros?', end=' ')
+metros = float(input())
+cent = metros*100
+mm = metros*1000
+print('Conversão realizada, {} metro(s) é equivalente a {} centimetros e {} milímetros.\n\n'.format(metros, cent, mm))
+
+des = ' DESAFIO 09 '
+print('{:#^30}'.format(des))
+print('Qual número você deseja saber a tabuada? ', end='')
+num = int(input())
+print('{} X 1 = {}'.format(num,num*1))
+print('{} X 2 = {}'.format(num,num*2))
+print('{} X 3 = {}'.format(num,num*3))
+print('{} X 4 = {}'.format(num,num*4))
+print('{} X 5 = {}'.format(num,num*5))
+print('{} X 6 = {}'.format(num,num*6))
+print('{} X 7 = {}'.format(num,num*7))
+print('{} X 8 = {}'.format(num,num*8))
+print('{} X 9 = {}'.format(num,num*9))
+print('{} X 10 = {}\n\n'.format(num,num*10))
+
+des = ' DESAFIO 010 '
+print('{:#^30}'.format(des))
+
+print('Quantos reais você quer converter para dólar? R$', end='')
+reais = float(input())
+dol = reais/4.95
+print('O Dólar está equivalente a R$4,95, portanto você possui: ${:.2f}\n\'.format(dol))
+
+des = ' DESAFIO 011 '
+print('{:@^30}'.format(des))
+print('Qual a largura da parede a ser pintada? ', end='')
+larg = float(input())
+print('Qual a altura da parede a ser pintada? ', end='')
+alt = float(input())
+parede = alt*larg
+print('A parede possui {:.2f}m² e 1l de tinta pinta 2m² portanto você precisara de {} litros de tinta.\n\n'.format(parede, parede/2))
+
+des = ' DESAFIO 012 '
+print('{:~^30}'.format(des))
+
+print('Quanto custa o produto? R$', end='')
+preco = float(input())
+print('Com o desconto de 5% o produto passa a valer: R${:.2f}\n\n'.format(preco-(preco/100)*5))
+
+des = ' DESAFIO 013 '
+print('{:/^30}'.format(des))
+
+print('Qual é o seu atual salário? R$', end='')
+salario = float(input())
+print('Com os 15% de aumento você passa a receber: R${:.2f}'.format(salario+(salario/100)*15))
